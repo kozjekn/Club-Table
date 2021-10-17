5655db1c7f358e8635075e74a46fa446bef1ca2a
nejek16 <kozjek.nejc198@gmail.com>
2021-10-17 16:30:36 +0200

    finished

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const Gpio = require('onoff').Gpio;
+const but_hand = new Gpio(4, 'in', 'both'); 
+const but_drink = new Gpio(17, 'in', 'both'); 
+
 const fs = require('fs');
 const httpServer = require("http").createServer(function (req, res) {
     
@@ -22,69 +26,24 @@ const io = require("socket.io")(httpServer, {
     }
 });
 
-
-
-io.on("connection", (socket) => {
-    try{
-        // Identifies mani socket (socket that plays music)
-        socket.on("main_auth",(arg) => {
-            socket.join("main_sockets");
-        });
-
-        // Identifies mani socket (socket that plays music)
-        socket.on("client_auth",(arg) => {
-            socket.join("clients");
-        });
-
-        //CLIENT MTHODS
-        socket.on("requestData", (arg) =>{ socket.emit('data', JSON.stringify(videoQueue)); });
-        socket.on("addVideo", (url) =>{
-            let video = videoQueue.addVideo(url);
-            if(video != null)
-            {
-                storedVideos[video.videoCode] = video;
-                fs.writeFile('./storedData.json', JSON.stringify(storedVideos),() =>{
-
-                });
-            }            
-            io.local.emit('data', JSON.stringify(videoQueue)); 
-        });
-
-        socket.on("addRandomVideos", (num) =>{
-            addRandomVideos(num);
-            io.local.emit('data', JSON.stringify(videoQueue)); 
-            
-        });
-
-        
-
-        socket.on("removeVideo", (videoID) =>{ 
-            videoQueue.removeVideo(videoID); 
-            io.local.emit('data', JSON.stringify(videoQueue)); 
-            if(videoQueue.videos.length == 0){
-                addRandomVideos(1);
-                io.local.emit('data', JSON.stringify(videoQueue));
-            }
-        });
-        socket.on("stop", (arg) => { io.local.emit('stop',''); })
-        socket.on("start", (arg) => { io.local.emit('start',''); })
-        socket.on("mute", (arg) => { io.local.emit('mute',''); })
-        socket.on("unmute", (arg) => { io.local.emit('unmute',''); })
-        socket.on("move", (arg) => { io.local.emit('move',arg); })
-        socket.on("volume", (arg) => { io.local.emit('volume',arg); })
-        socket.on("next", (arg) => { io.local.emit('next',''); })
-
-        //MAIN SOCKET METHODS
-        socket.on("currData", (_currVideo) =>{ 
-
-            io.local.emit("newCurrData", _currVideo);
-        });
-    }catch(e){ console.log(e); }
-});
-
-  
-
-
 //SOCKET STARTUP
 httpServer.listen(PORT_SOCKET);
 console.log('Listening on port '+ PORT_SOCKET);
+
+
+//EVENT
+but_hand.watch(function (err, value) { 
+    if (err) { //if an error
+      console.error('There was an error', err); 
+    return;
+    }
+    if(value) { io.local.emit("button", "handGame"); }
+  });
+
+but_drink.watch(function (err, value) { 
+    if (err) { //if an error
+      console.error('There was an error', err); 
+    return;
+    }
+    if(value) { io.local.emit("button", "drinkigGame"); }
+  });
