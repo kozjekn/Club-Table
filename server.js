13e32bb10819c26b1d4011562c55b5b1b16e95c6
nejek16 <kozjek.nejc198@gmail.com>
2021-10-17 17:34:32 +0200

    fixed http server

--- server.js
+++ server.js
@@ -3,16 +3,54 @@ const but_hand = new Gpio(4, 'in', 'both');
 const but_drink = new Gpio(17, 'in', 'both'); 
 
 const fs = require('fs');
-const httpServer = require("http").createServer(function (req, res) {
+const path = require('path');
+const httpServer = require("http").createServer(function (request, response) {
     
-    fs.readFile('./wwwroot/index.html', function (err,data) {
-      if (err) {
-        res.writeHead(404);
-        res.end(JSON.stringify(err));
-        return;
-      }
-      res.writeHead(200);
-      res.end(data);
+    var filePath = './wwwroot' + request.url;
+    if (filePath == './wwwroot/')
+        filePath = './wwwroot/index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
     });
 });
 
@@ -50,4 +88,4 @@ but_drink.watch(function (err, value) {
 
 
 
-  require('child_process').exec("chromium-browser http://localhost:3000 --kiosk");
+  //require('child_process').exec("chromium-browser http://localhost:3000 --kiosk");
